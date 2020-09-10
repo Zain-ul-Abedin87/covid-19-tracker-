f548a76e0ec30efa0b67ac46af63a9a3ae4850c6
Zain-ul-Abedin87 <66695724+Zain-ul-Abedin87@users.noreply.github.com>
2020-09-10 21:48:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+// import {f} from 
 
 function App() {
   return (
     <div className="app">
       <h1>Lets Build COVID-19 Tracker with the help of Allah Almighty</h1>
+      <formControl className="app__dropdown">
+        <select variant="outlined"
+        value="Abc"
+        >
+          <menuitem value="wordWide">WordWide</menuitem>
+          <menuitem value="wordWide">WordWide</menuitem>
+          <menuitem value="wordWide">WordWide</menuitem>
+          <menuitem value="wordWide">WordWide</menuitem>
+
+        </select>
+      </formControl>
 
     </div>
   );
