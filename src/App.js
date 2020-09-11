d78bca6f28e84de41dcbd44fd2ef2164e52e0e3f
Zain-ul-Abedin87 <66695724+Zain-ul-Abedin87@users.noreply.github.com>
2020-09-11 22:25:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { FormControl, Select, MenuItem } from "@material-ui/core";
 
@@ -6,29 +6,38 @@ function App() {
   const [countries, setcountries] = useState([
     "Pakistan",
     "Turki",
-    "Afganistan"
+    "Afganistan",
   ]);
-//  useEffect is mainly  use run a pice of code according to Ginven Condtaion 
+  //  useEffect is mainly  use run a pice of code according to Ginven Condtaion
   useEffect(() => {
-      // inside Code will b run once when the Componet loads not  agian
-      // asnync is use here becase data is fetch from server
-      // send request wait for it ,And do SomeThing
+    // inside Code will b run once when the Componet loads not  agian
+    // asnync is use here becase data is fetch from server
+    // send request wait for it ,And do SomeThing
 
-      const getCountriesData= async ()=>{
-        await fetch('https://disease.sh/v3/covid-19/countries')
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
         .then((response) => response.json())
-        .then((data))
-      }
-  }, [])
+        .then((data) => {
+          const countryDetails = data.map((items) =>(
+            {
+              name:items.country,
+              value:items.countryInfo.ios3
+            }
+          ) 
+          
+          );
+        });
+    };
+  }, []);
   return (
     <div className="app">
       <div className="app__header">
         <h1>COVID-19</h1>
         <FormControl className="app__dropdown">
           <Select variant="outlined" value="Abc">
-            {countries.map((item) => 
+            {countries.map((item) => (
               <MenuItem value={item}>{item}</MenuItem>
-            )}
+            ))}
           </Select>
         </FormControl>
       </div>
