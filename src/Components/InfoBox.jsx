4439c3e1866d01b99c387c6fc1722c3a95ae846a
Zain-ul-Abedin87 <66695724+Zain-ul-Abedin87@users.noreply.github.com>
2020-09-13 02:01:42 +0500

    Create InfoBox.jsx

---
+++ src/Components/InfoBox.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {Card,CardContent} from "@material-ui/core"
+
+function infoBox({ title, cases, total }) {
+  return(
+    <Card>
+        <CardContent>
+            {/* Tilte */}
+            {/* Number of Cases */}
+            {/* Total */}
+        </CardContent>
+    </Card>
+    )
+
+      
+}
+
+export default infoBox;
