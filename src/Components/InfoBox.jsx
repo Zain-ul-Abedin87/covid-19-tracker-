1847e4011277fb11b3d0ecb50a024d4baa7cf867
Zain-ul-Abedin87 <66695724+Zain-ul-Abedin87@users.noreply.github.com>
2020-09-16 00:19:56 +0500

    Update InfoBox.jsx

--- src/Components/InfoBox.jsx
+++ src/Components/InfoBox.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import {Card,CardContent} from "@material-ui/core"
+import { Card, CardContent, Typography } from "@material-ui/core";
 
-function infoBox({ title, cases, total }) {
-  return(
-    <Card>
-        <CardContent>
-            {/* Tilte */}
-            {/* Number of Cases */}
-            {/* Total */}
-        </CardContent>
+function InfoBox({ title, cases, total }) {
+  return (
+    <Card className="infoBox">
+      <CardContent>
+        <Typography className="infoBox_title" color="textSecondary">
+          {title}
+        </Typography>
+        <h2 className="infoBox-cases">{cases}</h2>
+        <Typography className="infoBox-total">{total} total</Typography>
+      </CardContent>
     </Card>
-    )
-
-      
+  );
 }
 
-export default infoBox;
+export default InfoBox;
