ff2357a7bc2c53b2d10b15eef8cacd0ffb1cf8fc
Zain-ul-Abedin87 <66695724+Zain-ul-Abedin87@users.noreply.github.com>
2020-10-09 21:46:59 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   Select,
   MenuItem,
   Card,
-  CardContent,
+  CardContent,Table
 } from "@material-ui/core";
 import InfoBox from "./Components/InfoBox";
 import Map from "./Components/Map";
@@ -13,6 +13,18 @@ import Map from "./Components/Map";
 function App() {
   const [countries, setcountries] = useState([]);
   const [country, setCountry] = useState("wordWide");
+  const [countryInfo, setcountryInfo] = useState({});
+  const [tableData,setTableData] =useState([])
+
+   useEffect(()=>{
+     fetch ("https://disease.sh/v3/covid-19/all")
+     .then((response)=> response.json())
+     .then((data)=>{
+       setTableData(data)
+       setcountryInfo(data)
+     })
+   },[])
+  
   //  useEffect is mainly  use run a pice of code according to Ginven Condtaion
   useEffect(() => {
     // inside Code will b run once when the Componet loads not  agian
@@ -34,17 +46,46 @@ function App() {
     getCountriesData();
   }, []);
 
-  const onCountryChange = (e) => {
+  const onCountryChange = async (e) => {
     const countryCode = e.target.value;
     console.log(countryCode);
     setCountry(countryCode);
+
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setcountryInfo(data);
+      });
   };
+  console.log(Object.values(countryInfo).map((item) => item.flag));
 
   return (
     <div className="app">
       <div className="app_left">
         <div className="app__header">
-          <h1>COVID-19</h1>
+          <h1 className="covid-heading">
+            Covid-19  Info About <span></span>
+            <span className="country-heading">
+              {countryInfo.country}
+              {Object.values(countryInfo).map(
+                (item) => {
+                  if (item.flag) {
+                    return <img src={item.flag} className="country_flag"></img>;
+                  } else {
+                    return console.log("nae mila");
+                  }
+                }
+                // console.log(item.flag)
+              )}
+            </span>
+          </h1>
+
           <FormControl className="app__dropdown">
             <Select
               variant="outlined"
@@ -61,15 +102,28 @@ function App() {
           </FormControl>
         </div>
         <div className="app-status">
-          <InfoBox title="corona-cases" total={2000} cases={123} />
-          <InfoBox title="Recoverd " total={2000} cases={123} />
-          <InfoBox title="Details" total={2000} cases={123} />
+          <InfoBox
+            title="CoronaVirus"
+            cases={countryInfo.todayCases}
+            total={countryInfo.active}
+          />
+          <InfoBox
+            title="Recoverd People "
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBox
+            title="No Of Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
         </div>
         <Map />
       </div>
       <Card>
         <CardContent>
-          <h1>new Cases</h1>
+          <h3>Live Cases by Country</h3>
+          <Table/>
           <h4>World Wide Cases</h4>
         </CardContent>
       </Card>
